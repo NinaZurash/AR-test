32b50ce9f72ece7691b3edcb8afb513f04de7649
nzurashv <nino.zurashvili@cern.ch>
2023-07-03 10:15:42 +0400

    test2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import './style.css'
+AFRAME.registerComponent('smooth-position', {
+  init: function () {
+    var model = this.el.object3D
+    var smoothedPosition = model.position.clone()
+
+    this.el.addEventListener('loaded', function () {
+      this.sceneEl.addEventListener('renderstart', function () {
+        var delta = this.clock.getDelta()
+        smoothedPosition.lerp(model.position, delta * 100) // Adjust the factor as per your requirement
+        model.position.copy(smoothedPosition)
+      })
+    })
+  },
+})
